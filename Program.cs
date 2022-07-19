08411151edb3a75fba99c6c78a2c5abf3e0ea09b
Polyanskaya Anastaiya Ruslanovna <98982139+PolyanskayaAR@users.noreply.github.com>
2022-07-19 14:20:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace modul_15
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            
+            IEnumerable<string> studentsAll = new List<string>();
+            foreach (var eachClass in classes)
+            {
+                studentsAll = studentsAll.Concat(eachClass.Students);
+            }
+            
+            var studentsAllFinal = studentsAll.ToArray();
+            return studentsAllFinal;
+        }
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
